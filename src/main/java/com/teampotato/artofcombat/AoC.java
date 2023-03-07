9aa102cf5c128ff951bc332b04c7c4d0153ffc10
Kasualix <670067575@qq.com>
2023-03-07 13:17:30 +0800

    init

---
+++ src/main/java/com/teampotato/artofcombat/AoC.java
@@ -0,0 +1,11 @@
+package com.teampotato.artofcombat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AoC.MOD_ID)
+public class AoC {
+    public static final String MOD_ID = "artofcombat";
+    public AoC() {
+        System.out.println("wHo YoU gOoNa cAlL?");
+    }
+}
