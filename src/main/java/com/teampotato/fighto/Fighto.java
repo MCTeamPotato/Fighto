143a2debfd3a1c4a25d9769373805cc2f0b18132
Kasualix <670067575@qq.com>
2023-03-08 17:05:55 +0800

    change id

---
+++ src/main/java/com/teampotato/fighto/Fighto.java
@@ -0,0 +1,11 @@
+package com.teampotato.fighto;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Fighto.MOD_ID)
+public class Fighto {
+    public static final String MOD_ID = "fighto";
+    public Fighto() {
+        System.out.println("wHo YoU gOoNa cAlL?");
+    }
+}
