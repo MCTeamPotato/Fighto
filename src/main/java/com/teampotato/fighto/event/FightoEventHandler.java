bfd447e709fa697fe18aeb165409cb65a43b2e6a
Kasualix <670067575@qq.com>
2023-03-08 22:30:52 +0800

    fix npe

--- src/main/java/com/teampotato/fighto/event/FightoEventHandler.java
+++ src/main/java/com/teampotato/fighto/event/FightoEventHandler.java
@@ -3,20 +3,20 @@ package com.teampotato.fighto.event;
 import com.teampotato.fighto.Fighto;
 import com.teampotato.fighto.util.CompoundTagUtil;
 import net.minecraft.client.gui.screens.inventory.EnchantmentScreen;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.*;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.Objects;
-
 @Mod.EventBusSubscriber
 public class FightoEventHandler {
-
     @SubscribeEvent
     public static void onItemIntoEnchantSlot(GuiScreenEvent.MouseClickedEvent.Pre event) {
         if (!(event.getGui() instanceof EnchantmentScreen)) return;
-        ItemStack itemStack = Objects.requireNonNull(((EnchantmentScreen) event.getGui()).getSlotUnderMouse()).getItem();
+        Slot slot = ((EnchantmentScreen) event.getGui()).getSlotUnderMouse();
+        if (slot == null) return;
+        ItemStack itemStack = slot.getItem();
         if (itemStack.isEmpty()) return;
         Item item = itemStack.getItem();
         if (Fighto.UNBREAKABLE_WEAPONS_AND_ARMORS.get() && !itemStack.getOrCreateTag().contains("Unbreakable")
